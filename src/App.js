0f63e82123213a84f01be53f3bf76c487dd3f95c
Kevin Vuong <kkvuong@ucdavis.edu>
2020-08-05 21:33:07 -0700

    centered, somewhat styled circles

--- src/App.js
+++ src/App.js
@@ -13,75 +13,42 @@ import { makeStyles } from "@material-ui/styles";
 import ShareIcon from "@material-ui/icons/Share";
 import EditOutlinedIcon from "@material-ui/icons/EditOutlined";
 
-const useStyles = makeStyles({
-  image: {
-    height: 500,
-  },
-});
+import Introduction from "./components/Introduction";
+import Section from "./components/Section";
+
+const DESCRIPTION_TEXT = `I love talking about the Asian American Experience in a medical lens.
+Let's connect!`;
 
 function App() {
-  const classes = useStyles();
   return (
-    <Grid container direction="column" justify="center" alignItems="center">
-      <Container>
-        <Typography variant="h1">James Do</Typography>
-        <Typography variant="subtitle">
-          I love talking about the Asian American Experience in a medical lens.
-          Let's connect!
-        </Typography>
-        <div>
-          <ShareIcon />
-          <EditOutlinedIcon />
-        </div>
-      </Container>
-      <Container style={{ flex: 1 }}>
-        <Typography variant="h6">Organizations</Typography>
-        <Card>
-          <CardActionArea>
-            <CardMedia
-              className={classes.image}
-              image={require("./assets/blm.jfif")}
-            />
-          </CardActionArea>
-        </Card>
-        <Card>
-          <CardActionArea>
-            <CardMedia
-              className={classes.image}
-              image={require("./assets/blm.jfif")}
-            />
-          </CardActionArea>
-        </Card>{" "}
-        <Card>
-          <CardActionArea>
-            <CardMedia
-              className={classes.image}
-              image={require("./assets/blm.jfif")}
-            />
-          </CardActionArea>
-        </Card>{" "}
-        <Card>
-          <CardActionArea>
-            <CardMedia
-              className={classes.image}
-              image={require("./assets/blm.jfif")}
-            />
-          </CardActionArea>
-        </Card>{" "}
-        <Card>
-          <CardActionArea>
-            <CardMedia
-              className={classes.image}
-              image={require("./assets/blm.jfif")}
-            />
-            <CardContent>
-              <Typography variant="h5">Black Lives Matter</Typography>
-              <Typography variant="h6">Involvement</Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      </Container>
-    </Grid>
+    <div>
+      <header>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        <Grid container direction="column" justify="center" alignItems="center">
+          <Introduction name="James Do" description={DESCRIPTION_TEXT} />
+          <Section title="Description" />
+        </Grid>
+      </header>
+    </div>
+    //   <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
