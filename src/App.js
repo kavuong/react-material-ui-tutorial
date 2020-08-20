978c24d6602fd2958ce24f3582b05479c7226435
Kevin Vuong <kkvuong@ucdavis.edu>
2020-08-19 23:03:01 -0700

    added picture and share icons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from "react";
 import "./App.css";
-import { Grid, Typography, Container } from "@material-ui/core";
+import { Grid, Typography, Container, IconButton } from "@material-ui/core";
 import ShareIcon from "@material-ui/icons/Share";
 import EditOutlinedIcon from "@material-ui/icons/EditOutlined";
 
 function App() {
   return (
-    <Grid container direction="column" justify="center" alignItems="center">
-      <Container>
-        <Typography variant="h1">James Do</Typography>
-        <Typography variant="subtitle">
-          I love talking about the Asian American Experience in a medical lens.
-          Let's connect!
-        </Typography>
-        <div>
-          <ShareIcon />
-          <EditOutlinedIcon />
-        </div>
-      </Container>
-    </Grid>
+    // Container: justifies all content towards the center of the page
+    <Container>
+      {/* Grid with container attribute makes a grid that abides by the flex layout model */}
+      <Grid container>
+        <Grid item xs={12} sm={6}>
+          <Typography variant="h1">James Do</Typography>
+          <Typography variant="subtitle">
+            I love talking about the Asian American Experience in a medical
+            lens. Let's connect!
+          </Typography>
+          <div>
+            <IconButton>
+              <ShareIcon />
+            </IconButton>
+            <IconButton>
+              <EditOutlinedIcon />
+            </IconButton>
+          </div>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <img src="https://dawn-advocacy.s3-us-west-1.amazonaws.com/sample-images/Rectangle+64.png"></img>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
